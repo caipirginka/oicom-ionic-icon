48e8ce1d1b6dd5b7a16b149b8b8b2de2f2546aa5
caipirginka <github@oicom.com>
2020-03-15 15:08:32 +0100

    Removed less useful functions.

--- projects/oicom-icon/src/lib/oicom-icon.service.ts
+++ projects/oicom-icon/src/lib/oicom-icon.service.ts
@@ -23,26 +23,10 @@ export class OicomIconService {
           ...this._assignIcon(name, prefer ? `${name}-${prefer}` : name),
           ...this._assignIcon(`${name}-fill`, name),
           ...this._assignIcon(`${name}-outline`),
-          ...this._assignIcon(`${name}-sharp`),
-          /*           [`${name}`]: m[_.camelCase(name)],
-                    [`${name}-outline`]: m[_.camelCase(`${name}-outline`)],
-                    [`${name}-sharp`]: m[_.camelCase(`${name}-sharp`)], */
+          ...this._assignIcon(`${name}-sharp`)
         }
       }, {}),
     });
   }
 
-  public addAlias(name: string, md: string, ios: string) {
-    ioniconsAddIcons({
-      ...this._assignIcon(`md-${name}`, md),
-      ...this._assignIcon(`ios-${name}`, ios),
-    });
-  }
-
-  public addPlatform(name: string, md: string, ios: string) {
-    ioniconsAddIcons({
-      ...this._assignIcon(`md-${name}`, md),
-      ...this._assignIcon(`ios-${name}`, ios),
-    });
-  }
 }
